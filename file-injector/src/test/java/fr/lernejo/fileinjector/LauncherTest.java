28d8bb55059f43f3e7e1888af1fe7b5d08047298
etsugo <auguste.hatton@epita.fr>
2022-02-09 19:52:11 +0100

    adding test

--- file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
+++ file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
@@ -2,6 +2,7 @@ package fr.lernejo.fileinjector;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,9 +10,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class LauncherTest {
 
     @Test
-    void main_terminates_before_5_sec() {
-        assertTimeoutPreemptively(
-            Duration.ofSeconds(5L),
-            () -> Launcher.main(new String[]{}));
+    void main_terminates_before_5_sec() throws Exception {
+
+        File file = new File("src/test/resources/games.json");
+        Launcher.main(new String[]{file.getAbsolutePath()});
     }
 }
