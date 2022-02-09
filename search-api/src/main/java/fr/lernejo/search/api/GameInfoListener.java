f89c684652afd81d83d62da12569a570fc0c1e10
etsugo <auguste.hatton@epita.fr>
2022-02-09 02:04:06 +0100

    adding game listener

--- search-api/src/main/java/fr/lernejo/search/api/GameInfoListener.java
+++ search-api/src/main/java/fr/lernejo/search/api/GameInfoListener.java
@@ -1,4 +1,41 @@
 package fr.lernejo.search.api;
 
-public class GameInfoListener {
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexRequestBuilder;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.xcontent.XContentType;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GameInfoListener
+{
+
+    private final RestHighLevelClient restHighLevelClient;
+
+    public GameInfoListener(RestHighLevelClient restHighLevelClient) {
+        this.restHighLevelClient = restHighLevelClient;
+    }
+
+    @RabbitListener(queues = AmqpConfiguration.GAME_INFO_QUEUE)
+    public void onMessage(String message, @Header("game_id") String gameId)
+    {
+        IndexRequest indexRequest = new IndexRequest();
+
+        indexRequest.index("games");
+        indexRequest.id(gameId);
+        indexRequest.source(XContentType.JSON, message);
+
+        try {
+            restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
