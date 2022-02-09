528e916e43de4b0e769100193f561c5a458d4732
etsugo <auguste.hatton@epita.fr>
2022-02-09 19:21:53 +0100

    adding game model

--- file-injector/src/main/java/fr/lernejo/fileinjector/GameModel.java
+++ file-injector/src/main/java/fr/lernejo/fileinjector/GameModel.java
@@ -1,4 +1,17 @@
 package fr.lernejo.fileinjector;
 
+
 public class GameModel {
+
+    public Integer id;
+    public String title;
+    public String thumbnail;
+    public String short_description;
+    public String genre;
+    public String platform;
+    public String publisher;
+    public String developer;
+    public String release_date;
+    public String profile;
+    public String game;
 }
